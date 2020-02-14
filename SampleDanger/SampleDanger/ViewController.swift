dd0287b54b3f8326daecbd85aa2a372495e09286
Udit Ajmera <udit.ajmera@mhealth.tech>
2020-02-14 14:36:03 +0530

    added space

--- SampleDanger/SampleDanger/ViewController.swift
+++ SampleDanger/SampleDanger/ViewController.swift
@@ -12,8 +12,11 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    print("check")
     // Do any additional setup after loading the view.
   }
+  
+  
 
 
 }
