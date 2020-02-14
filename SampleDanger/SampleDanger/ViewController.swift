696910d5faeece7ed73c4cbc7d001a9ecb0094ea
Udit Ajmera <udit.ajmera@mhealth.tech>
2020-02-14 14:37:41 +0530

    sfddbgr

--- SampleDanger/SampleDanger/ViewController.swift
+++ SampleDanger/SampleDanger/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    print("check")
+    print("checkdfgh")
     // Do any additional setup after loading the view.
   }
   
