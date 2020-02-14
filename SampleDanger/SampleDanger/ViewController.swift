0c1e22c7853eed9478db5b743c4a88e367e3a20e
Udit Ajmera <udit.ajmera@mhealth.tech>
2020-02-14 14:30:49 +0530

    added initial setup

---
+++ SampleDanger/SampleDanger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleDanger
+//
+//  Created by Udit Ajmera on 14/02/20.
+//  Copyright © 2020 Halodoc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
