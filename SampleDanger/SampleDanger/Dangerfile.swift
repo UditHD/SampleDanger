dbd5f021a7df665c86775f7cffb36f322f6db472
Udit Ajmera <udit.ajmera@mhealth.tech>
2020-02-16 15:05:33 +0530

    added swiftlint

--- SampleDanger/SampleDanger/Dangerfile.swift
+++ SampleDanger/SampleDanger/Dangerfile.swift
@@ -5,9 +5,23 @@ let hasChangelogEntry = danger.git.modifiedFiles.contains("CHANGELOG.md")
 
 // Example from danger
 // Let people bail from the CHANGELOG check
-let hasSkipChangelog = danger.github.pullRequest.body.contains("#no_changelog")
+let hasSkipChangelog = danger.github.pullRequest.body?.contains("#no_changelog")
 
 // Request for a CHANGELOG entry with each app change
-if editedFiles.count > 0 && !hasChangelogEntry && !hasSkipChangelog {
+if editedFiles.count > 0 && !hasChangelogEntry && !(hasSkipChangelog ?? false) {
     fail("Please add a CHANGELOG entry for these changes. If you would like to skip this check, add `#no_changelog` to the PR body and re-run CI.")
+}
+
+let hasJiraID = danger.github.pullRequest.body?.contains("DDI")
+if !(hasJiraID ?? false) {
+    fail("I couldn't find the Jira ticket ID! Did you forget to add it?")
+}
+
+let editedTravisFile = editedFiles.filter { $0.contains(".travis.yml") }
+if !editedTravisFile.isEmpty {
+    warn("Did you really mean to edit the travis.yml file?")
+}
+
+if danger.github.pullRequest.user.login == "dagostini" {
+    warn("Not this guy again!")
 }
