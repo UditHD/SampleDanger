18f2521b902725085825d9302ceab321114a696f
Udit Ajmera <udit.ajmera@mhealth.tech>
2020-02-25 09:14:10 +0530

    fixed issues

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,6 +1,23 @@
-import Danger 
+import Danger
+import Foundation
+
 let danger = Danger()
 //SwiftLint.lint()
+if danger.git.createdFiles.count + danger.git.modifiedFiles.count - danger.git.deletedFiles.count > 300 {
+    warn("Big PR, try to keep changes smaller if you can")
+}
+
+let swiftFilesWithCopyright = danger.git.createdFiles.filter {
+    $0.fileType == .swift
+        && danger.utils.readFile($0).contains("//  Created by")
+}
+
+if !swiftFilesWithCopyright.isEmpty {
+    let files = swiftFilesWithCopyright.joined(separator: ", ")
+    warn("In Danger JS we don't include copyright headers, found them in: \(files)")
+}
+
+//SwiftLint.lint(.modifiedAndCreatedFiles(directory: "Sources"), inline: true)
 SwiftLint.lint(inline: true)
 // Instead of making a markdown table in the main message
 // sprinkle those comments inline, this can be a bit noisy
